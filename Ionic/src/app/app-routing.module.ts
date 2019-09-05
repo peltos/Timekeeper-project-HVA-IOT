192bfb07ab09e069f6bc747a0f1163be14c5a0a7
Ron Pelt <pelt8@hotmail.com>
2019-09-05 12:36:48 +0200

    added login page

--- Ionic/src/app/app-routing.module.ts
+++ Ionic/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [
