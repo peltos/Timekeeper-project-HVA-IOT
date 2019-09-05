6deb56a16ff4b24662a3eb8f04ef4e2a6f508a10
Stefan Katers <stefan.katers@pxlwidgets.com>
2019-09-05 22:08:03 +0200

    Added cors header

--- server/GetTasks.php
+++ server/GetTasks.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
 $conn = new mysqli('localhost', 'root', 'test123', 'tasks');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
