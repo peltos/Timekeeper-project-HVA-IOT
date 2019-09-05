6f29f4784ac4ee3ddeb29371dcaa2a48bc8af75d
Ron Pelt <pelt8@hotmail.com>
2019-09-05 10:13:05 +0200

    added server file

---
+++ server/add_task.php
@@ -0,0 +1,26 @@
+<?php
+$db_handle = new mysqli('localhost', 'root', 'test123', 'tasks');
+if (mysqli_connect_errno()) {
+    die("MySQL connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_GET['id'])) {
+	$id = $_GET['id'];
+}
+
+if (isset($_GET['hash'])){
+	$hash = $_GET['hash'];
+}
+
+if (isset($_GET['tap'])) {
+    $tap = $_GET['tap'];
+    if ($tap == 'start') {
+		$query = "INSERT INTO tasks (id, hash, start_time) VALUES ('" . $id . "', " . $hash . ", CURRENT_TIMESTAMP)";
+	}
+	if ($tap == 'end') {
+		$query = "UPDATE tasks SET end_time=CURRENT_TIMESTAMP WHERE id='" . $id . "' AND hash=" . $hash . "";
+	}
+} 
+echo($query);
+$result = $db_handle->query($query);
+?>
