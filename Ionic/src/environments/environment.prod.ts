3e90f2a999f028cff4ec33ab47ac6065bbbfa324
Stefan Katers <stefan.katers@pxlwidgets.com>
2019-09-05 20:04:23 +0200

    Changed API endpoint

--- Ionic/src/environments/environment.prod.ts
+++ Ionic/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const API_ENDPOINT = 'http://157.245.69.229:8000';
+export const API_ENDPOINT = 'http://157.245.70.238:8000';
 export const API_TIMEOUT = 5000;
--- Ionic/src/environments/environment.ts
+++ Ionic/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const API_ENDPOINT = 'http://157.245.69.229:8000';
+export const API_ENDPOINT = 'http://157.245.70.238:8000';
 export const API_TIMEOUT = 5000;
 
 /*
